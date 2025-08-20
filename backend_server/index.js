3d08684a174cc6ce12f5f62c79c4f8092d9c9bdc
Anupriya Dayaratne <anupriya.dayaratne@gmail.com>
2025-08-20 20:47:44 +1200

    more codes added

---
+++ backend_server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+//try endpoints
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
